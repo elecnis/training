103baff1100997958c5a1c8b01bc80220bcdfc2b
Walid El Sayed Aly <walid.elsayedaly@1und1.de>
2016-02-09 17:21:41 +0100

    initial source project

---
+++ sources/gitTrain/src/main/java/com/oneandone/comet/App.java
@@ -0,0 +1,13 @@
+package com.oneandone.comet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
